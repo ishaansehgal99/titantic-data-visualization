82a00645913b0fde209e3cb6d604542a3b7de3f6
Ishaan Sehgal <ishaanforthewin@gmail.com>
2019-04-19 23:46:42 -0500

    Included CSS file

--- visualization.js
+++ visualization.js
@@ -13,18 +13,18 @@ $(function() {
 
 var visualize = function(data) {
   // Boilerplate:
-  var margin = { top: 50, right: 50, bottom: 50, left: 50 },
-     width = 960 - margin.left - margin.right,
-     height = 500 - margin.top - margin.bottom;
-
-  var svg = d3.select("#chart")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .style("width", width + margin.left + margin.right)
-    .style("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  // var margin = { top: 50, right: 50, bottom: 50, left: 50 },
+  //    width = 960 - margin.left - margin.right,
+  //    height = 500 - margin.top - margin.bottom;
+  //
+  // var svg = d3.select("#chart")
+  //   .append("svg")
+  //   .attr("width", width + margin.left + margin.right)
+  //   .attr("height", height + margin.top + margin.bottom)
+  //   .style("width", width + margin.left + margin.right)
+  //   .style("height", height + margin.top + margin.bottom)
+  //   .append("g")
+  //   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
   // Visualization Code:
 
